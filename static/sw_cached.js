1a488dfdf07e1662ea9113bc4493bad49b610fb5
Afiqah Azhar <18FTT1715@student.pb.edu.bn>
2020-04-12 02:13:44 +0800

    commit

--- static/sw_cached.js
+++ static/sw_cached.js
@@ -1,30 +1,8 @@
-const cacheName = 'v1';
-
-const cacheAssets = [
-    '/default/baseof.html',
-    '/default/list.html',
-    '/default/single.html',
-    '/partials/footer.html',
-    '/partials/seo.html',
-    '/partials/sidebar.html',
-    '/partials/theme-color',
-    '/404.html',
-    '/index.html',
-];
+const cacheName = 'v2';
 
 // Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
-
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        console.log('Service Worker: Caching Files');
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
 });
 
 // Call Activate Event
@@ -48,5 +26,18 @@ self.addEventListener('activate', e => {
 // Call Fetch Event
 self.addEventListener('fetch', e => {
   console.log('Service Worker: Fetching');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Make copy/clone of response
+        const resClone = res.clone();
+        // Open cahce
+        caches.open(cacheName).then(cache => {
+          // Add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+  );
 });
